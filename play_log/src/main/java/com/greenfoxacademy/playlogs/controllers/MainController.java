aa8dbc8b45c75dcf16b1faca155810dc4f7611fc
respublika <teglas.szilvia@gmail.com>
2018-08-01 23:52:53 +0200

    Add log test project

---
+++ play_log/src/main/java/com/greenfoxacademy/playlogs/controllers/MainController.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy.playlogs.controllers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    static Logger log = LogManager.getLogger(MainController.class);
+
+    @GetMapping(value = "/")
+    public String main() {
+        log.info("valami");
+        return "hello world";
+
+    }
+}
